d6efc43eb523b2b8e2374ef4eba061d5fe0943ba
Razzwan <razvanlomov@gmail.com>
2024-05-17 22:57:22 +0300

    remove unwrap to see errors

--- src/apis/mod.rs
+++ src/apis/mod.rs
@@ -29,46 +29,49 @@ const AUDIO_TRANSLATIONS_CREATE: &str = "audio/translations";
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Usage {
-	pub prompt_tokens: Option<u32>,
-	pub completion_tokens: Option<u32>,
-	pub total_tokens: Option<u32>,
+    pub prompt_tokens: Option<u32>,
+    pub completion_tokens: Option<u32>,
+    pub total_tokens: Option<u32>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Choice {
-	pub text: Option<String>,
-	pub index: u32,
-	pub logprobs: Option<String>,
-	pub finish_reason: Option<String>,
-	pub message: Option<Message>,
+    pub text: Option<String>,
+    pub index: u32,
+    pub logprobs: Option<String>,
+    pub finish_reason: Option<String>,
+    pub message: Option<Message>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Message {
-	pub role: Role,
-	pub content: String,
+    pub role: Role,
+    pub content: String,
 }
 
 impl Clone for Message {
-	fn clone(&self) -> Self {
-		Self { role: self.role.clone(), content: self.content.clone() }
-	}
+    fn clone(&self) -> Self {
+        Self {
+            role: self.role.clone(),
+            content: self.content.clone(),
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum Role {
-	System,
-	Assistant,
-	User,
+    System,
+    Assistant,
+    User,
 }
 
 impl Clone for Role {
-	fn clone(&self) -> Self {
-		match self {
-			Self::System => Self::System,
-			Self::Assistant => Self::Assistant,
-			Self::User => Self::User,
-		}
-	}
+    fn clone(&self) -> Self {
+        match self {
+            Self::System => Self::System,
+            Self::Assistant => Self::Assistant,
+            Self::User => Self::User,
+        }
+    }
 }
--- src/apis/models.rs
+++ src/apis/models.rs
@@ -16,70 +16,70 @@ use super::MODELS_RETRIEVE;
 /// to understand what models are available and the differences between them.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Model {
-	pub id: String,
-	pub object: Option<String>,
-	pub owned_by: Option<String>,
-	pub permission: Vec<Permission>,
+    pub id: String,
+    pub object: Option<String>,
+    pub owned_by: Option<String>,
+    pub permission: Vec<Permission>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Permission {
-	pub id: String,
-	pub object: Option<String>,
-	pub created: u64,
-	pub allow_create_engine: bool,
-	pub allow_sampling: bool,
-	pub allow_logprobs: bool,
-	pub allow_search_indices: bool,
-	pub allow_view: bool,
-	pub allow_fine_tuning: bool,
-	pub organization: Option<String>,
-	pub group: Option<String>,
-	pub is_blocking: bool,
+    pub id: String,
+    pub object: Option<String>,
+    pub created: u64,
+    pub allow_create_engine: bool,
+    pub allow_sampling: bool,
+    pub allow_logprobs: bool,
+    pub allow_search_indices: bool,
+    pub allow_view: bool,
+    pub allow_fine_tuning: bool,
+    pub organization: Option<String>,
+    pub group: Option<String>,
+    pub is_blocking: bool,
 }
 
 pub trait ModelsApi {
-	/// Lists the currently available models,
-	/// and provides basic information about each one such as the owner and availability.
-	fn models_list(&self) -> ApiResult<Vec<Model>>;
-	/// Retrieves a model instance,
-	/// providing basic information about the model such as the owner and permissioning.
-	fn models_retrieve(&self, model_id: &str) -> ApiResult<Model>;
+    /// Lists the currently available models,
+    /// and provides basic information about each one such as the owner and availability.
+    fn models_list(&self) -> ApiResult<Vec<Model>>;
+    /// Retrieves a model instance,
+    /// providing basic information about the model such as the owner and permissioning.
+    fn models_retrieve(&self, model_id: &str) -> ApiResult<Model>;
 }
 
 impl ModelsApi for OpenAI {
-	fn models_list(&self) -> ApiResult<Vec<Model>> {
-		let res: Json = self.get(MODELS_LIST)?;
-		let data = res.as_object().unwrap().get("data");
-		if let Some(data) = data {
-			let models: Vec<Model> = serde_json::from_value(data.clone()).unwrap();
-			return Ok(models);
-		}
-		Err(Error::ApiError("No data".to_string()))
-	}
+    fn models_list(&self) -> ApiResult<Vec<Model>> {
+        let res: Json = self.get(MODELS_LIST)?;
+        let data = res.as_object().unwrap().get("data");
+        if let Some(data) = data {
+            let models: Vec<Model> = serde_json::from_value(data.clone()).unwrap();
+            return Ok(models);
+        }
+        Err(Error::ApiError("No data".to_string()))
+    }
 
-	fn models_retrieve(&self, model_id: &str) -> ApiResult<Model> {
-		let res: Json = self.get(&(MODELS_RETRIEVE.to_owned() + model_id))?;
-		let model: Model = serde_json::from_value(res).unwrap();
-		Ok(model)
-	}
+    fn models_retrieve(&self, model_id: &str) -> ApiResult<Model> {
+        let res: Json = self.get(&(MODELS_RETRIEVE.to_owned() + model_id))?;
+        let model: Model = serde_json::from_value(res).unwrap();
+        Ok(model)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use crate::{apis::models::ModelsApi, openai::new_test_openai};
+    use crate::{apis::models::ModelsApi, openai::new_test_openai};
 
-	#[test]
-	fn test_models() {
-		let openai = new_test_openai();
-		let models = openai.models_list().unwrap();
-		assert!(!models.is_empty());
-	}
+    #[test]
+    fn test_models() {
+        let openai = new_test_openai();
+        let models = openai.models_list().unwrap();
+        assert!(!models.is_empty());
+    }
 
-	#[test]
-	fn test_get_model() {
-		let openai = new_test_openai();
-		let model = openai.models_retrieve("babbage").unwrap();
-		assert_eq!("babbage", model.id);
-	}
+    #[test]
+    fn test_get_model() {
+        let openai = new_test_openai();
+        let model = openai.models_retrieve("babbage").unwrap();
+        assert_eq!("babbage", model.id);
+    }
 }
